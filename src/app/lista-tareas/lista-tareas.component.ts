3496624475d5a29297cbd7746f6a7eb584a548c3
Juan Cambizaca <juan.cambizaca@ucuenca.edu.ec>
2024-06-24 12:42:11 -0500

    J.C ADD: edit and save to Tareas

--- src/app/lista-tareas/lista-tareas.component.ts
+++ src/app/lista-tareas/lista-tareas.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { DialogModule } from 'primeng/dialog';
+import { InputTextModule } from 'primeng/inputtext';
 
 interface Tarea {
   id: number;
@@ -10,13 +13,15 @@ interface Tarea {
 @Component({
   selector: 'app-lista-tareas',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, ButtonModule, DialogModule, InputTextModule],
   templateUrl: './lista-tareas.component.html',
   styleUrls: ['./lista-tareas.component.css']
 })
 export class ListaTareasComponent {
   tareas: Tarea[] = [];
   nuevaTarea: string = '';
+  dialogVisible: boolean = false;
+  tareaEditando: Tarea = { id: 0, nombre: '' };
   
   agregarTarea() {
     if (this.nuevaTarea.trim()) {
@@ -24,5 +29,22 @@ export class ListaTareasComponent {
       this.tareas.push({ id: nuevaId, nombre: this.nuevaTarea.trim() });
       this.nuevaTarea = '';
     }
+  }
+
+  editarTarea(tarea: Tarea) {
+    this.tareaEditando = { ...tarea };
+    this.dialogVisible = true;
+  }
+
+  guardarEdicion() {
+    const index = this.tareas.findIndex(t => t.id === this.tareaEditando.id);
+    if (index !== -1) {
+      this.tareas[index] = { ...this.tareaEditando };
+    }
+    this.dialogVisible = false;
+  }
+
+  eliminarTarea(id: number) {
+    this.tareas = this.tareas.filter(tarea => tarea.id !== id);
   }
 }
